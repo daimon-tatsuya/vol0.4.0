2d1b54a6649d7c8f8ea4d3357c5ccb6bd02c29d8
daimon <d.tatsuya326@gmail.com>
2020-01-21 18:45:39 +0900

    spritedataの設定,ゲームタイムが0になるとscene_overに行く

---
+++ pause.cpp
@@ -0,0 +1,78 @@
+#include "all.h"
+
+using namespace system;
+static int PauseCount = 0;
+void Pause::init()
+{
+    pause[0].data = &sprTitle_single;
+    pause[1].data = &sprTitle_doubles;
+    pause[3].data = &sprTitle_doubles;
+    position.y = (SCREEN_HEIGHT / 2.0f);
+    position.x = (SCREEN_WIDTH / 2.0f);
+    size = VECTOR2(27, 32 - 2); //スケールは当たり判定の値なので実際の大きさの半分を入れる
+    color = VECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+    exist = true;
+    no = 0;
+    pause[0].scale = { 0.5f,0.5f };
+    pause[1].scale = { 0.45f,0.45f };
+    pause[2].scale = { 0.45f,0.45f };
+}
+void Pause::update()
+{
+
+    if (TRG(0) & PAD_TRG2)//ゲームを開始
+    {
+        title_state = 2;
+    }
+
+    if (TRG(0) &  PAD_UP)
+    {
+        PauseCount--;
+    }
+
+    if (TRG(0) &PAD_DOWN)
+    {
+        PauseCount++;
+    }
+
+    if (PauseCount < 0)
+    {
+        PauseCount = 0;
+    }
+    if (PauseCount > 2)
+    {
+        PauseCount = 2;
+    }
+
+    if (PauseCount == 0)
+    {
+        
+        pause[0].scale = { 0.75f,0.75f };
+        pause[1].scale = { 0.45f,0.45f };
+        pause[2].scale = { 0.45f,0.45f };
+    }
+
+    if (PauseCount == 1)
+    {
+        
+        pause[0].scale = { 0.45f,0.45f };
+        pause[1].scale = { 0.75f,0.75f };
+        pause[2].scale = { 0.45f,0.45f };
+    }
+
+    if (PauseCount == 2)
+    {
+       
+
+        pause[0].scale = { 0.45f,0.45f };
+        pause[1].scale = { 0.45f,0.45f };
+        pause[2].scale = { 0.75f,0.75f };
+    }
+}
+void Pause::draw()
+{
+    if (data)
+    {
+        data->draw(position, scale, angle, color);
+    }
+}
